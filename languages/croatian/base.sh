70475ab719b9d240d29ac2b517da4ab2ecade598
sheep <sheep@sheepdev.xyz>
2022-12-19 22:36:18 +0100

    fix(typo): fix grammar in translation

--- languages/croatian/base.sh
+++ languages/croatian/base.sh
@@ -6,7 +6,7 @@ Dobrodošli u NixMember, linux skriptu koja vam pomaže da instalirate i konfigu
 
 Namjenjena je za početnike u Linux svijetu.
 
-Program je napravljen od strane NixMember tima, a možete ga pronaći na https://github.com/sheeepdev/NixMember
+Program je napravljen od strane NixMember tima, a možete nas pronaći na https://github.com/sheeepdev/NixMember
 NixMember tim se sastoji od 2 člana, a to su:
 - Jakov Koprivnjak
 - Lukas Hajneman
