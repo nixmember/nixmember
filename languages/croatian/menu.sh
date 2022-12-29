e0053e06e81d1884a534bf3ce41cfb1e397835c9
sheep <sheep@sheepdev.xyz>
2022-12-29 13:49:49 +0100

    feat(setup): add link

--- languages/croatian/menu.sh
+++ languages/croatian/menu.sh
@@ -4,13 +4,13 @@ echo "Odaberite opciju: "
 
 PS3="> "
 
-select lang in "Setup" "Info" "Quit"
+select option in "Setup" "Info" "Quit"
 do
-    case $lang in
-        "Setup" ) source languages/english/base.sh; break;;
-        "Info" ) source languages/german/base.sh; break;;
+    case $option in
+        "Setup" ) source setup.sh; break;;
+        "Info" ) source languages/croatian/base.sh; break;;
         "Quit" ) exit;;
-        * ) echo "Invalid option";;
+        * ) echo "Neponuđena opcija";;
     esac
 done
 
