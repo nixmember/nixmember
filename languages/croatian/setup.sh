19a0ce869aeaf4a0ca1c7bbdbc2376a7bd1a24c4
sheep <sheep@sheepdev.xyz>
2023-01-12 13:51:40 +0100

    frano miskovic

---
+++ languages/croatian/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Odaberite opciju: "
+
+PS3="> "
+
+select option in "Setup" "Info" "Quit"
+do
+    case $option in
+        "Setup" ) source setup.sh; break;;
+        "Info" ) source languages/croatian/base.sh; break;;
+        "Quit" ) exit;;
+        * ) echo "Neponuđena opcija";;
+    esac
+done
