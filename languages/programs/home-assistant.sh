f586cd75d5fa681e4bdd79a4099e63d514361dcd
sheep <sheep@sheepdev.xyz>
2023-02-16 20:53:24 +0100

    urgent

--- languages/programs/home-assistant.sh
+++ languages/programs/home-assistant.sh
@@ -2,23 +2,41 @@
 
 echo 'Sve bitne informacije biti će spremljene u $HOME/home-assistant'
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo apt install docker-compose
-
-mkdir $HOME/home-assistant
-mkdir $HOME/home-assistant/config
-cat > ~/home-assistant/docker-compose.yml << END
-version: '3'
-services:
-  homeassistant:
-    container_name: homeassistant
-    image: "ghcr.io/home-assistant/home-assistant:stable"
-    volumes:
-      - $HOME/home-assistant/config:/config
-      - /etc/localtime:/etc/localtime:ro
-    restart: unless-stopped
-    privileged: true
-    network_mode: host
+if [ -f "/usr/bin/docker" ]; then
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sudo sh get-docker.sh
+	sudo apt install docker-compose
+	mkdir $HOME/home-assistant
+	mkdir $HOME/home-assistant/config
+	cat > ~/home-assistant/docker-compose.yml << END
+	version: '3'
+	services:
+	  homeassistant:
+	    container_name: homeassistant
+	    image: "ghcr.io/home-assistant/home-assistant:stable"
+	    volumes:
+	      - $HOME/home-assistant/config:/config
+	      - /etc/localtime:/etc/localtime:ro
+	    restart: unless-stopped
+	    privileged: true
+	    network_mode: host
 END
-docker-compose up -d $HOME/home-assistant/docker-compose.yml
+	docker-compose up -d $HOME/home-assistant/docker-compose.yml
+else
+	mkdir $HOME/home-assistant
+	mkdir $HOME/home-assistant/config
+	cat > ~/home-assistant/docker-compose.yml << END
+	version: '3'
+	services:
+	  homeassistant:
+	    container_name: homeassistant
+	    image: "ghcr.io/home-assistant/home-assistant:stable"
+	    volumes:
+	      - $HOME/home-assistant/config:/config
+	      - /etc/localtime:/etc/localtime:ro
+	    restart: unless-stopped
+	    privileged: true
+	    network_mode: host
+END
+	docker-compose up -d $HOME/home-assistant/docker-compose.yml
+fi
--- languages/programs/jellyfin.sh
+++ languages/programs/jellyfin.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
 read -p 'Odaberite mapu koja sadrži vaše medije (filmove, serije)' mapa
-echo $mapa
 
+if [ -f "/usr/bin/docker" ]; then
+	echo 'Sve informacije o ovome programu bit će spremljene u $HOME/jellyfin'
+	mkdir $HOME/jellyfin
+	mkdir $HOME/jellyfin/config
+	mkdir $HOME/jellyfin/cache
+	cat > ~/home-assistant/docker-compose.yml << END
+	version: '3.5'
+	services:
+	  jellyfin:
+	    image: jellyfin/jellyfin
+	    container_name: jellyfin
+	    user: uid:gid
+	    network_mode: 'host'
+	    volumes:
+	      - $HOME/jellyfin/config:/config
+	      - $HOME/jellyfin/cache:/cache
+	      - $mapa:/media
+	    restart: 'unless-stopped'
+	    # Optional - alternative address used for autodiscovery
+	    environment:
+	      - JELLYFIN_PublishedServerUrl=http://example.com
+	    # Optional - may be necessary for docker healthcheck to pass if running in host network mode
+	    extra_hosts:
+	      - "host.docker.internal:host-gateway"
+END
+	docker-compose up -d $HOME/jellyfin/docker-compose.yml
+else
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo apt install docker-compose
@@ -32,3 +58,4 @@ services:
       - "host.docker.internal:host-gateway"
 END
 docker-compose up -d $HOME/jellyfin/docker-compose.yml
+fi
