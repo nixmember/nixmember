e7f5666a93b33689b02b269672ce84774735fa76
sheep <sheep@sheepdev.xyz>
2023-02-16 20:22:04 +0100

    urgent fixes

--- languages/croatian/setup.sh
+++ languages/croatian/setup.sh
@@ -4,12 +4,14 @@ echo "Odaberite program koji želite instalirati: "
 
 PS3="> "
 
+DIR="$( echo $(cd ../ && pwd) )/programs/"
+
 select option in "Bitwarden - centralno mjesto za sve vaše lozinke (instalacija samo na engleskom jeziku)" "Home Assistant - program koji vam pomaže upravljati svojim domom" "Jellyfin - svi vaši filmovi i serije na jednom mjestu!"
 do
     case $option in
-        "Bitwarden - centralno mjesto za sve vaše lozinke (instalacija samo na engleskom jeziku)" ) source ../programs/bitwarden.sh; break;;
-        "Home Assistant - program koji vam pomaže upravljati svojim domom" ) source ../programs/home-assistant.sh; break;;
-        "Jellyfin - svi vaši filmovi i serije na jednom mjestu!" ) source ../programs/jellyfin.sh; break;;
+        "Bitwarden - centralno mjesto za sve vaše lozinke (instalacija samo na engleskom jeziku)" ) source $DIR/bitwarden.sh; break;;
+        "Home Assistant - program koji vam pomaže upravljati svojim domom" ) source $DIR/home-assistant.sh; break;;
+        "Jellyfin - svi vaši filmovi i serije na jednom mjestu!" ) source $DIR/jellyfin.sh; break;;
         "Izlaz" ) exit;;
         * ) echo "Neponuđena opcija";;
     esac
