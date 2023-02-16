f3863bf86603252392a94ead5e70f16b454f4b99
sheep <sheep@sheepdev.xyz>
2023-02-16 21:26:28 +0100

    final few fixes

--- languages/german/menu.sh
+++ languages/german/menu.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-# translation soon
+echo "Choose a option: "
+
+PS3="> "
+
+select option in "Setup" "Info" "Quit"
+do
+    case $option in
+        "Setup" ) source languages/english/setup.sh; break;;
+        "Info" ) source languages/english/base.sh; break;;
+        "Quit" ) exit;;
+        * ) echo "No such option";;
+    esac
+done
+
